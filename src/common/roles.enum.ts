d3aa4fecd5986f64075c26d771327861e31a5586
bluebead421@gmail.com <dikshab463@gmail.com>
2026-02-13 14:14:40 +0530

    improved folder structure

--- src/common/roles.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum Role {
-    STUDENT = 'STUDENT',
-    FACULTY = 'FACULTY',
-    ADMIN = 'ADMIN',
-}
--- src/users/users.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './user.entity';
-
-@Injectable()
-export class UsersService {
-    
-}
